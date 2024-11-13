57752021684c13d266464f54d8350a374ad9dcf0
mashaklubnikova <159928202+mashaklubnikova@users.noreply.github.com>
2024-11-13 21:17:25 +0300

    лабораторная 4 исправленная

---
+++ конвертер1.py
@@ -0,0 +1,27 @@
+import csv
+import json
+
+INPUT_FILENAME = "input.csv"
+OUTPUT_FILENAME = "output.json"
+
+
+def task() -> None:
+    data = []
+    with open(INPUT_FILENAME, 'r', encoding='utf-8') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            data.append(row)
+
+    json_string = json.dumps(data, indent=4)
+
+    with open(OUTPUT_FILENAME, 'w', encoding='utf-8') as outfile:
+        outfile.write(json_string)
+
+
+if __name__ == '__main__':
+    # Нужно для проверки
+    task()
+
+    with open(OUTPUT_FILENAME) as output_f:
+        for line in output_f:
+            print(line, end="")
